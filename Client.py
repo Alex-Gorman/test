56005bf17c6f137f9b32675ddb03a9f812f6e5f1
ajg029 <a.gorman029@gmail.com>
2023-06-22 18:12:28 -0600

    Pushing

---
+++ Client.py
@@ -0,0 +1,107 @@
+import socket
+import time
+import random
+import numpy as np
+import sys
+import json
+
+def multiply_the_arrays(array_list, multiplier):
+    for i in range(len(array_list)):
+        for j in range(len(array_list[i])):
+            array_list[i][j] = array_list[i][j] + multiplier
+    
+    return array_list
+
+
+def main():
+
+    # Create TCP socket
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Connect the socket to the server's address and port
+    server_address = ('localhost', 30000)
+
+    # Try connecting the server
+    try:
+        client_socket.connect(server_address)
+    except ConnectionError as e:
+        print("Unable to connect to the server:", str(e))
+        sys.exit()
+
+    # Client connected to the server
+    print("Connected to server...")
+
+    # list of numpy arrays
+    array_list = []
+    lengths = [3, 3, 3]
+
+    # add the numpy arrays to the list
+    for length in lengths:
+        random_array = np.random.randint(1, 11, size=length)
+        
+        # Convert NumPy array to list since to can't convert NumPy array to JSON
+        array_list.append(random_array.tolist())
+
+    # set the print options to remove the delimiter space
+    np.set_printoptions(formatter={'int': lambda x: f'{x}'})
+
+    # print the numpy arrays to console
+    for array in array_list:
+        print(array)
+
+
+    try:
+        # Infinite loop
+        while True:
+
+            # Sleep for a random amount of time
+            time.sleep(random.randint(1, 10))
+
+            # Convert the list of NumPy arrays to a JSON string
+            array_list_json = json.dumps(array_list)
+
+            # Send the JSON string to the server
+            client_socket.sendall(array_list_json.encode())
+
+            # Receive the response from the server
+            try: 
+                data = client_socket.recv(1024)
+                if not data:
+                    print("Connection error with Server detected. Exciting program. ")
+                    sys.exit()
+
+                # Split the received data into individual JSON objects
+                received_data = data.decode()
+                json_objects = received_data.split("\n")
+
+                for json_obj in json_objects:
+                    if json_obj:
+                        # Print the response from the server
+                        print(f'Received from server: {json_obj}')
+
+                        # Get the 3x3 array list from the server
+                        array_list = json.loads(json_obj)
+                        
+                        # Randomly multiply each element in the array list by 0.1 to 1
+                        for i in range(len(array_list)):
+                            for j in range(len(array_list[i])):
+                                multiplier = random.uniform(0.1, 1)
+                                array_list[i][j] *= multiplier
+                                array_list[i][j] = round(array_list[i][j], 1)
+
+
+            except ConnectionError:
+                print("Connection error with Server detected. Exciting program. ")
+                sys.exit()
+    
+
+    # If you get a keyboard interrupt then exit the program
+    except KeyboardInterrupt:
+        print("Keyboard interrupt detected. Exciting Program.")
+        sys.exit()
+
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ Server.py
@@ -0,0 +1,163 @@
+import socket
+import select
+import json
+import sys
+import numpy as np
+
+def calculate_average(arrays):
+    total_sum = 0
+    total_elements = 0
+    for array in arrays:
+        for element in array:
+            total_sum += element
+            total_elements += 1
+    if total_elements > 0:
+        return total_sum / total_elements
+    else:
+        return 0
+
+def main():
+    print("Server Started.")
+    num_clients = 0
+    num_of_iterations = 0
+
+    # Get the number of clients
+    if len(sys.argv) > 3 or len(sys.argv) < 3:
+        print("Usage: python3 Server.py numberOfClients numberOfIterations")
+        sys.exit()
+    else:
+        num_clients = int(sys.argv[1])
+        num_of_iterations = int(sys.argv[2])
+
+    # Create TCP socket
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    # Bind the socket to a specific address and port
+    server_address = ('localhost', 30000)
+    server_socket.bind(server_address)
+
+    # Listen for incoming connections
+    server_socket.listen(num_clients)
+
+    # List of sockets for select.select()
+    sockets_list = [server_socket]
+
+    print("Server listening for incoming connections...")
+
+    # average over the arrays to be used as a multiplier
+    average = 1
+
+    # keep track of connected clients
+    connected_clients = []
+
+    # accept all the clients before trying to receive messages from them
+    try:
+        while len(connected_clients) < num_clients:
+            readable_sockets, _, _ = select.select(sockets_list, [], [])
+
+            for sock in readable_sockets:
+                if sock == server_socket:
+                    client_socket, client_address = server_socket.accept()
+                    sockets_list.append(client_socket)
+                    connected_clients.append(client_socket)
+                    print(f'New client connected: {client_address}')
+    
+    except KeyboardInterrupt:
+        print("Keyboard interruption detected. Exciting the program. ")
+        sys.exit()
+
+    
+    print("All clients connected. Listening for messages...")
+
+    array_3x3 = []
+    for _ in range(3):
+        row = []
+        for _ in range(3):
+            row.append(0)
+        array_3x3.append(row)
+
+
+    # print(array_3x3)
+
+    client_count = 0
+    iterations_count = 0
+
+
+    try: 
+        while True:
+            if iterations_count == num_of_iterations:
+                print("Iterations number met. Exciting system")
+                sys.exit()
+
+
+            readable_sockets, _, _ = select.select(sockets_list, [], [])
+
+            # Iterate over the sockets that are ready to be read
+            for sock in readable_sockets:
+                # If a new connection is received, accept it and add it to the list of sockets
+                if sock == server_socket:
+                    client_socket, client_address = server_socket.accept()
+                    sockets_list.append(client_socket)
+                    print(f'New client connected: {client_address}')
+
+                # Otherwise, receive data from the connected client    
+                else:
+                    data = sock.recv(1024)
+
+                    # If no data is received, remove the socket from the list of sockets
+                    if not data:
+                        sockets_list.remove(sock)
+                        print(f'Client disconnected: {sock.getpeername()}')
+                        print("One client has disconnected. Exciting the program. ")
+                        sys.exit()
+                    
+                    else:
+                        client_count += 1
+                        receive_data = data.decode()
+                        array_list = json.loads(receive_data)
+
+                        # Print each array from the list
+                        for array in array_list:
+                            print(array)
+                        
+                        for i in range(len(array_list)):
+                            for j in range(len(array_list[i])):
+                                array_3x3[i][j] += array_list[i][j]
+                        
+                        # print(array_3x3)
+                        print(" ")
+
+                        if (client_count < num_clients):
+                            continue
+                        else:
+                            
+                            client_count = 0
+                            iterations_count += 1
+
+                            for i in range(len(array_3x3)):
+                                for j in range(len(array_3x3)):
+                                    array_3x3[i][j] /= num_clients
+                                    array_3x3[i][j] = round(array_3x3[i][j], 1)
+                            
+                            print(array_3x3)
+                            print(" ")
+
+                            # Convert the list of arrays to a JSON string
+                            array_json = json.dumps(array_3x3)
+                            array_list_json = f"{array_json}\n"
+
+
+                            # Send the JSON string of 3x3 array to each client
+                            for clientSocket in connected_clients:
+                                clientSocket.sendall(array_list_json.encode())
+
+    
+    except KeyboardInterrupt:
+        print("Keyboard interrupt detected. Exciting the Program.")
+        sys.exit()
+            
+
+
+if __name__ == '__main__':
+    main()
